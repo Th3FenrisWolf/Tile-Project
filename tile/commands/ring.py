3aa8153db46a4e560a1945c0eb3c4829364413c1
Tim Dibert <49311697+dubbltninja@users.noreply.github.com>
2022-01-16 16:00:30 -0500

    Put Songs class in ring.py

--- tile/commands/ring.py
+++ tile/commands/ring.py
@@ -1,2 +1,27 @@
 #class Songs(enum.IntEnum)
 #class Ring(Command)
+
+class Songs:
+    CLICK_1      = 0x00
+    FIND         = 0x01
+    ACTIVE       = 0x02
+    SLEEP        = 0x03
+    WAKEUP       = 0x04
+    FACTORY_TEST = 0x05
+    MYSTERY      = 0x06
+    SILENT       = 0x07
+    BUTTON       = 0x08
+    WAKEUP_PART  = 0x09
+    DT_SUCCESS   = 0x0a
+    DT_FAILURE   = 0x0b
+    CLICK_2      = 0x0c
+    BIP_1        = 0x0d
+    BIP_2        = 0x0e
+    BIP_3        = 0x0f
+    BIP_4        = 0x10
+    BIP_5        = 0x11
+    BIP_6        = 0x12
+    BIP_7        = 0x13
+    DT_HB        = 0x14
+    MAX          = 0x15
+    STOP         = 0xFF
--- tile/tile.py
+++ tile/tile.py
@@ -47,30 +47,7 @@ class Toa_Cmd_Code:
     TPC           = 0x19
     ASSOCIATE     = 0x1A
 
-class Tile_Song:
-    CLICK_1      = 0x00
-    FIND         = 0x01
-    ACTIVE       = 0x02
-    SLEEP        = 0x03
-    WAKEUP       = 0x04
-    FACTORY_TEST = 0x05
-    MYSTERY      = 0x06
-    SILENT       = 0x07
-    BUTTON       = 0x08
-    WAKEUP_PART  = 0x09
-    DT_SUCCESS   = 0x0a
-    DT_FAILURE   = 0x0b
-    CLICK_2      = 0x0c
-    BIP_1        = 0x0d
-    BIP_2        = 0x0e
-    BIP_3        = 0x0f
-    BIP_4        = 0x10
-    BIP_5        = 0x11
-    BIP_6        = 0x12
-    BIP_7        = 0x13
-    DT_HB        = 0x14
-    MAX          = 0x15
-    STOP         = 0xFF
+
 
 class Tile:
 
@@ -80,7 +57,7 @@ class Tile:
     done = False
 
     @classmethod
-    async def create(self, mac_address: str, auth_key: bytes):
+    async def create(self, mac_address: str, auth_key: bytes = None):
         self = Tile()
         self.auth_key = auth_key
 
@@ -97,6 +74,7 @@ class Tile:
                     bleakClientSetup = True
                     break
         
+        #connect using Bleak API's connect; attempt to catch exceptions
         while not tileFound:
             print('Connecting To Tile...')
             try:
