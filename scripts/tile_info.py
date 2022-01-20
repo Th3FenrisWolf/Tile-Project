6b45e9d92f721ca814c911a1f1b6734f0cb2be6c
Ryan Albrecht <ralbrecht@cedarvill.edu>
2022-01-20 15:08:58 -0500

    fixed bugs in tdi

--- scripts/tile_info.py
+++ scripts/tile_info.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append("/home/tile/Desktop/cedarville-2021/tile")
 from tile import Tile
 
 tile = Tile("e6:9e:55:1a:91:28")  # TDI command doesn't require a channel, so auth key isn't needed
