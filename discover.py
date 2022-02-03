5305ed7cb1c384dee677dccf62adcdf7335e76d3
Tim Dibert <49311697+dubbltninja@users.noreply.github.com>
2022-02-03 15:17:45 -0500

    Made discover scripts report dynamically
    Madeline's original findTiles has now become findTilesLegacy

--- discover.py
+++ discover.py
@@ -1,9 +1,48 @@
 import asyncio
 from bleak import BleakScanner
+import sys
 
-async def run():
-    devices = await BleakScanner.discover()
-    for d in devices:
-        print(d)
+# usage:
+# python3 discover.py [search time] [address]
 
-asyncio.run(run())
+devices_found = 0
+addr_list = []
+search_addr = None
+
+def detection_callback(device, advertisement_data):
+    global search_addr
+    global addr_list
+    global devices_found
+    if search_addr == None:
+        if device.address not in addr_list:
+            devices_found += 1
+            addr_list.append(device.address)
+            print(device.address, "RSSI:", device.rssi, advertisement_data)
+    elif device.address == search_addr:
+        print("Search Device Found!")
+        print(device.address, "RSSI:", device.rssi, advertisement_data)
+        sys.exit(0)
+    else:
+        devices_found += 1
+        print("Found a device (", devices_found, ")")
+
+async def main(time = 30.0, addr = None):
+    args = sys.argv[1:]
+    if len(args) == 0:
+        print("Searching for all BT devices for", time, "seconds...")
+    elif len(args) == 1:
+        time = float(args[0])
+        print("Searching for all BT devices for", time, "seconds...")
+    elif len(args) == 2:
+        time = float(args[0])
+        addr = float(args[1])
+        global search_addr
+        search_addr = addr
+        print("Searching for address", search_addr, "for", time, "seconds...")
+    scanner = BleakScanner()
+    scanner.register_detection_callback(detection_callback)
+    await scanner.start()
+    await asyncio.sleep(time)
+    await scanner.stop()
+
+asyncio.run(main())
--- findTiles.py
+++ findTiles.py
@@ -1,17 +1,45 @@
 import asyncio
-from asyncio.windows_events import NULL
 from bleak import BleakScanner
+import sys
 
-async def main():
-    #devices = await BleakScanner.discover()
-    tileUUID = "0000feed-0000-1000-8000-00805f9b34fb"
-    metadatatile = "{'uuids': ['0000feed-0000-1000-8000-00805f9b34fb']"
-    async with BleakScanner() as scanner:
-        await asyncio.sleep(45.0)
-        #find each discoverable device
-        for d in scanner.discovered_devices:
-            #if it's a Tile based on the UUID then display it
-            if tileUUID in d.metadata["uuids"]:
-                print("NAME: ", d.name, " ADDRESS: ", d.address, " METADATA: ", d.metadata)
+# usage:
+# python3 findTiles.py [address] [search time]
+
+addr_list = []
+search_addr = None
+tileUUID = "0000feed-0000-1000-8000-00805f9b34fb"
+
+def detection_callback(device, advertisement_data):
+    global search_addr
+    global addr_list
+    global tileUUID
+    if search_addr == None:
+        if tileUUID in device.metadata["uuids"]:
+            addr_list.append(device.address)
+            print("NAME:", device.name, " ADDRESS:", device.address, " RSSI:", device.rssi, advertisement_data)
+    elif device.address == search_addr and tileUUID in device.metadata["uuids"]:
+        print("Tile Found!")
+        print("NAME:", device.name, " ADDRESS:", device.address, " RSSI:", device.rssi, advertisement_data)
+        sys.exit(0)
+
+async def main(addr = None, time = 30.0):
+    args = sys.argv[1:]
+    global search_addr
+    if len(args) == 0:
+        print("Searching for all Tiles for", time, "seconds...")
+    elif len(args) == 1:
+        addr = args[0]
+        search_addr = addr
+        print("Searching for Tile w/ address", search_addr, "for", time, "seconds...")
+    elif len(args) == 2:
+        addr = args[0]
+        search_addr = addr
+        time = float(args[1])
+        print("Searching for Tile w/ address", search_addr, "for", time, "seconds...")    
+    scanner = BleakScanner()
+    scanner.register_detection_callback(detection_callback)
+    await scanner.start()
+    await asyncio.sleep(time)
+    await scanner.stop()
 
 asyncio.run(main())
