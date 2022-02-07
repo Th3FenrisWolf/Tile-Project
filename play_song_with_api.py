92181538f0a563065e004c548e54270bb063edd0
Tim Dibert <49311697+dubbltninja@users.noreply.github.com>
2022-02-06 22:36:21 -0500

    Added back missing files

---
+++ play_song_with_api.py
@@ -0,0 +1,34 @@
+from logging import exception
+import sys
+import asyncio
+import bleak
+
+import tile_api
+import time
+
+# specifc to our tile, acquired from ble scanning and verified in disassembly
+MAC_ADDRESS = "E6:9E:55:1A:91:28"
+# wallet
+# MAC_ADDRESS = "C8:DE:26:A7:E1:42"
+
+
+# specifc to our tile, in disassembly from toa_module struct
+AUTH_KEY = b"\x59\xbe\xca\x33\xac\x3d\x4a\x65\xc7\x1e\xeb\xca\x8d\x91\x8b\x77"
+
+
+
+async def main(): 
+    args = sys.argv[1:]
+    if len(args) != 2:
+        print("usage: play_song_with_api.py <song_number> <strength>")
+        sys.exit(1)
+
+    songToPlay = int(args[0], 16)
+    strengthToPlay = int(args[1])
+
+    myTile = await tile_api.Tile.create(MAC_ADDRESS, AUTH_KEY)
+
+    await myTile.open_channel()
+    await myTile.play_song(songToPlay, strengthToPlay)
+
+asyncio.run(main())
